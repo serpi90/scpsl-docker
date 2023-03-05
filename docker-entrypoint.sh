87ccdd85e4e63b65041ddc921d96d520c3906a62
Julián Maestri <serpi90@gmail.com>
2023-03-05 16:24:01 -0300

    :bug: handle EULA question

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,6 +13,22 @@ GAME_CONFIG_PATH="/home/steam/.config/SCP Secret Laboratory/config"
 
 echo "Setup directory structure and permissions"
 mkdir -p "$GAME_CONFIG_PATH" "$CONFIG_PATH" "$INSTALL_PATH"
+
+if [ "$EULA" != "yes" ]; then
+	echo "You must accept de EULA by setting the env var EULA = yes in order to continue"
+	exit 1
+fi
+
+echo "Accepting EULA"
+echo "{
+	\"GitHubPersonalAccessToken\":null,
+	\"EulaAccepted\":\"$(date --utc --iso=seconds | sed 's/+00:00$/Z/')\",
+	\"PluginManagerWarningDismissed\":false,
+	\"LastPluginAliasesRefresh\":null,
+	\"PluginVersionCache\":{},
+	\"PluginAliases\":{}
+}" > "$GAME_CONFIG_PATH/localadmin_internal_data.json"
+
 ln -sf "$CONFIG_PATH" "$GAME_CONFIG_PATH/$PORT"
 chown -R steam:steam "$INSTALL_PATH" "$CONFIG_PATH" /home/steam/
 
